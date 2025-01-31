154330784c6080a7512d3b521b154343877a10ab
vonssy <kak.reyvaldizakaria@gmail.com>
2025-01-31 13:42:20 +0700

    Update Loop Interval

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ from aiohttp import (
     ClientSession,
     ClientTimeout
 )
-from colorama import *
-from datetime import datetime, timedelta, timezone
 from fake_useragent import FakeUserAgent
-import asyncio, random, json, time, base64, os, pytz
+from datetime import datetime, timedelta, timezone
+from colorama import *
+import asyncio, json, time, base64, os, pytz
 
 wib = pytz.timezone('Asia/Jakarta')
 
@@ -48,25 +48,30 @@ class Functor:
         hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
-    
-    def hide_token(self, token):
-        hide_token = token[:3] + '*' * 3 + token[-3:]
-        return hide_token
 
-    def hide_email(self, email):
-        if '@' not in email:
-            hide_email = email[:3] + '*' * 3 + email[-3:]
-            return hide_email
-        else:
-            local, domain = email.split('@', 1)
-            hide_local = local[:3] + '*' * 3 + local[-3:]
-            return f"{hide_local}@{domain}"
+    def mask_account(self, account):
+        if '@' in account:
+            local, domain = account.split('@', 1)
+            mask_account = local[:3] + '*' * 3 + local[-3:]
+            return f"{mask_account}@{domain}"
+
+        mask_account = account[:3] + '*' * 3 + account[-3:]
+        return mask_account
+            
     
     def decode_token(self, token: str):
-        header, payload, signature = token.split(".")
-        decoded_payload = base64.urlsafe_b64decode(payload + "==").decode("utf-8")
-        parsed_payload = json.loads(decoded_payload)
-        return parsed_payload
+        try:
+            header, payload, signature = token.split(".")
+            decoded_payload = base64.urlsafe_b64decode(payload + "==").decode("utf-8")
+            parsed_payload = json.loads(decoded_payload)
+
+            username = parsed_payload['email']
+            user_id = parsed_payload['sub']
+            exp_time = parsed_payload['exp']
+
+            return self.mask_account(username), user_id, exp_time
+        except Exception as e:
+            return None, None, None
 
     async def user_data(self, token: str, retries=5):
         url = 'https://node.securitylabs.xyz/api/v1/users'
@@ -77,22 +82,16 @@ class Functor:
         }
         for attempt in range(retries):
             try:
-                async with ClientSession(timeout=ClientTimeout(total=20)) as session:
+                async with ClientSession(timeout=ClientTimeout(total=30)) as session:
                     async with session.get(url=url, headers=headers) as response:
                         response.raise_for_status()
                         return await response.json()
             except (Exception, ClientResponseError) as e:
                 if attempt < retries - 1:
-                    print(
-                        f"{Fore.RED + Style.BRIGHT}ERROR.{Style.RESET_ALL}"
-                        f"{Fore.YELLOW + Style.BRIGHT} Retrying.... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
-                        end="\r",
-                        flush=True
-                    )
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                    await asyncio.sleep(5)
+                    continue
+
+                return None
                 
     async def user_checkin(self, token: str, user_id: str, retries=5):
         url = f'https://node.securitylabs.xyz/api/v1/users/earn/{user_id}'
@@ -103,128 +102,90 @@ class Functor:
         }
         for attempt in range(retries):
             try:
-                async with ClientSession(timeout=ClientTimeout(total=20)) as session:
+                async with ClientSession(timeout=ClientTimeout(total=30)) as session:
                     async with session.get(url=url, headers=headers) as response:
                         response.raise_for_status()
                         return await response.json()
             except (Exception, ClientResponseError) as e:
                 if attempt < retries - 1:
-                    print(
-                        f"{Fore.RED + Style.BRIGHT}ERROR.{Style.RESET_ALL}"
-                        f"{Fore.YELLOW + Style.BRIGHT} Retrying.... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
-                        end="\r",
-                        flush=True
-                    )
-                    await asyncio.sleep(2)
-                else:
-                    return None
+                    await asyncio.sleep(5)
+                    continue
+                
+                return None
         
-    async def process_accounts(self, token: str):
-        hide_token = self.hide_token(token)
-        token_data = self.decode_token(token)
-        if token_data is None:
+    async def process_accounts(self, token: str, user_id: str, exp_time: int):
+        exp_time_wib = datetime.fromtimestamp(exp_time, pytz.utc).astimezone(wib).strftime('%x %X %Z')
+        if int(time.time()) > exp_time:
             self.log(
-                f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                f"{Fore.WHITE + Style.BRIGHT} Token {hide_token} {Style.RESET_ALL}"
-                f"{Fore.RED + Style.BRIGHT}Isn't Valid{Style.RESET_ALL}"
-                f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                f"{Fore.CYAN + Style.BRIGHT}Status  :{Style.RESET_ALL}"
+                f"{Fore.RED + Style.BRIGHT} Token Expired {Style.RESET_ALL}"
             )
             return
-        
-        if token_data and "exp" in token_data:
-            now = int(time.time())
-            exp_time = token_data['exp']
-            exp_time_wib = datetime.fromtimestamp(exp_time, pytz.utc).astimezone(wib).strftime('%x %X %Z')
-            if now >= exp_time:
+
+        self.log(
+            f"{Fore.CYAN + Style.BRIGHT}Status  :{Style.RESET_ALL}"
+            f"{Fore.GREEN + Style.BRIGHT} Token Active {Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT} Expired at {Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT}{exp_time_wib}{Style.RESET_ALL}"
+        )
+
+        balance = "N/A"
+        last_checkin = None
+
+        user = await self.user_data(token)
+        if user:
+            balance = user.get("dipTokenBalance")
+            last_checkin = user.get('dipInitMineTime')
+
+        self.log(
+            f"{Fore.CYAN + Style.BRIGHT}Balance :{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} {balance} Points {Style.RESET_ALL}"
+        )
+
+        if last_checkin is None:
+            check_in = await self.user_checkin(token, user_id)
+            if check_in:
                 self.log(
-                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} {hide_token} {Style.RESET_ALL}"
-                    f"{Fore.RED + Style.BRIGHT}Is Expired{Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                    f"{Fore.CYAN + Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                    f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                    f"{Fore.CYAN + Style.BRIGHT} Reward {Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT}{check_in['tokensToAward']} Points{Style.RESET_ALL}"
                 )
-                return
-            
             else:
                 self.log(
-                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} Token {hide_token} {Style.RESET_ALL}"
-                    f"{Fore.GREEN + Style.BRIGHT}Is Valid{Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT} ] [ Expired at{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} {exp_time_wib} {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    f"{Fore.CYAN + Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                    f"{Fore.RED + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
                 )
-                await asyncio.sleep(1)
-
-                user = await self.user_data(token)
-                if not user:
+        else:
+            now = datetime.utcnow().replace(tzinfo=timezone.utc)
+            last_checkin_utc = datetime.strptime(last_checkin, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+            next_checkin_utc = last_checkin_utc + timedelta(hours=24)
+            next_checkin_wib = next_checkin_utc.astimezone(wib).strftime('%x %X %Z')
+            if now >= next_checkin_utc:
+                check_in = await self.user_checkin(token, user_id)
+                if check_in:
                     self.log(
-                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT} Token {hide_token} {Style.RESET_ALL}"
-                        f"{Fore.RED + Style.BRIGHT}Data Is None{Style.RESET_ALL}"
-                        f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                        f"{Fore.CYAN + Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                        f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                        f"{Fore.CYAN + Style.BRIGHT} Reward {Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT}{check_in['tokensToAward']} Points{Style.RESET_ALL}"
                     )
-                    return
-                
-                if user:
-                    user_id = user['id']
-                    email = user['email']
-                    hide_email = self.hide_email(email)
+                else:
                     self.log(
-                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA + Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT} {user['dipTokenBalance']} Points {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                        f"{Fore.CYAN + Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
                     )
-                    await asyncio.sleep(1)
-
-                    last_checkin = user['dipInitMineTime']
-                    if last_checkin is None:
-                        check_in = await self.user_checkin(token, user_id)
-                        if check_in:
-                            self.log(
-                                f"{Fore.MAGENTA + Style.BRIGHT}[ Check-In{Style.RESET_ALL}"
-                                f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
-                                f"{Fore.WHITE + Style.BRIGHT} {check_in['tokensToAward']} Points {Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                            )
-                        else:
-                            self.log(
-                                f"{Fore.MAGENTA + Style.BRIGHT}[ Check-In{Style.RESET_ALL}"
-                                f"{Fore.RED + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                            )
-                    else:
-                        now = datetime.utcnow().replace(tzinfo=timezone.utc)
-                        last_checkin_utc = datetime.strptime(last_checkin, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
-                        next_checkin_utc = last_checkin_utc + timedelta(hours=24)
-                        next_checkin_wib = next_checkin_utc.astimezone(wib).strftime('%x %X %Z')
-                        if now >= next_checkin_utc:
-                            check_in = await self.user_checkin(token, user_id)
-                            if check_in:
-                                self.log(
-                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Check-In{Style.RESET_ALL}"
-                                    f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
-                                    f"{Fore.MAGENTA + Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
-                                    f"{Fore.WHITE + Style.BRIGHT} {check_in['tokensToAward']} Points {Style.RESET_ALL}"
-                                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                                )
-                            else:
-                                self.log(
-                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Check-In{Style.RESET_ALL}"
-                                    f"{Fore.RED + Style.BRIGHT} Isn't Claimed {Style.RESET_ALL}"
-                                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                                )
-                        else:
-                            self.log(
-                                f"{Fore.MAGENTA + Style.BRIGHT}[ Check-In{Style.RESET_ALL}"
-                                f"{Fore.YELLOW + Style.BRIGHT} Not Time to Claim {Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT}] [ Next Claim At{Style.RESET_ALL}"
-                                f"{Fore.WHITE + Style.BRIGHT} {next_checkin_wib} {Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                            )
+            else:
+                self.log(
+                    f"{Fore.CYAN + Style.BRIGHT}Check-In:{Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT} Is Already Claimed {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                    f"{Fore.CYAN + Style.BRIGHT} Next Claim at {Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT}{next_checkin_wib}{Style.RESET_ALL}"
+                )
 
     async def main(self):
         try:
@@ -238,26 +199,22 @@ class Functor:
                     f"{Fore.GREEN + Style.BRIGHT}Account's Total: {Style.RESET_ALL}"
                     f"{Fore.WHITE + Style.BRIGHT}{len(tokens)}{Style.RESET_ALL}"
                 )
-                self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
                 
+                separator = "=" * 25
                 for token in tokens:
-                    token = token.strip()
                     if token:
-                        await self.process_accounts(token)
-                        self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
-                        seconds = random.randint(15, 30)
-                        while seconds > 0:
-                            formatted_time = self.format_seconds(seconds)
-                            print(
-                                f"{Fore.CYAN+Style.BRIGHT}[ Wait for{Style.RESET_ALL}"
-                                f"{Fore.WHITE+Style.BRIGHT} {formatted_time} {Style.RESET_ALL}"
-                                f"{Fore.CYAN+Style.BRIGHT}... ]{Style.RESET_ALL}",
-                                end="\r"
+                        username, user_id, exp_time = self.decode_token(token)
+                        if username and user_id and exp_time:
+                            self.log(
+                                f"{Fore.CYAN + Style.BRIGHT}{separator}[{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {username} {Style.RESET_ALL}"
+                                f"{Fore.CYAN + Style.BRIGHT}]{separator}{Style.RESET_ALL}"
                             )
-                            await asyncio.sleep(1)
-                            seconds -= 1
+                            await self.process_accounts(token, user_id, exp_time)
+                            await asyncio.sleep(3)
 
-                seconds = 28800
+                self.log(f"{Fore.CYAN + Style.BRIGHT}={Style.RESET_ALL}" * 75)
+                seconds = 12 * 60 * 60
                 while seconds > 0:
                     formatted_time = self.format_seconds(seconds)
                     print(
